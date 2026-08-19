600248da4ad1f3bd61fa40105a1ad8dc496c4b77
Matheus Barbosa <139350778+MatheusBarbosaDeAndrade@users.noreply.github.com>
2026-08-19 04:10:28 +0000

    modificando mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sthefany, amo você. Se você me ama dá uma risadinha</h1>
+    return <h1>Sthefany, amo você. Se você me ama dá uma risadinha hehe (riu né?)</h1>
 }
 
 export default Home;
