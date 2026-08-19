b30fbc40e7c17ab34f9f9159d2717e65e6eb7e83
Matheus Barbosa <139350778+MatheusBarbosaDeAndrade@users.noreply.github.com>
2026-08-19 20:40:47 +0000

    adciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Enquanto eu trabalho, você passeia pelos meus pensamentos o tempo todo (que saudade) </h1>
+function Home() {
+  return (
+    <h1>
+      Enquanto eu trabalho, você passeia pelos meus pensamentos o tempo todo
+      (que saudade){" "}
+    </h1>
+  );
+}
+
+function Teste() {
+  console.log("teste");
 }
 
 export default Home;
